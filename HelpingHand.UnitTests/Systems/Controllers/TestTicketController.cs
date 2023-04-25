bd1c19a4470d5d5efb003421c30a92874941b437
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-04-25 22:37:23 +0200

    update unit tests

---
+++ HelpingHand.UnitTests/Systems/Controllers/TestTicketController.cs
@@ -0,0 +1,26 @@
+using FluentAssertions;
+using HelpingHand.API;
+using HelpingHand.API.Controllers;
+using HelpingHand.API.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace HelpingHand.UnitTests.Systems.Controllers
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void GetTicketById_ReturnsTicket()
+        {
+            //Arrange
+            var logger = new Mock<ILogger<TicketController>>();
+            var controller = new TicketController(logger.Object);
+
+            //Act
+            var result = controller.GetTicketById(0);
+
+            //Assert
+            result.Should().BeOfType<Ticket>();
+        }
+    }
+}
