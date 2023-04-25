e4b4486ce0cadb2f417cb91b7805a8584db01a4c
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-04-25 22:36:50 +0200

    add ticket controller placeholder

---
+++ HelpingHand.API/Controllers/TicketController.cs
@@ -0,0 +1,22 @@
+﻿using HelpingHand.API.Controllers.Interfaces;
+using HelpingHand.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpingHand.API.Controllers
+{
+	public class TicketController : ITicketController
+	{
+		private readonly ILogger<TicketController> _logger;
+
+		public TicketController(ILogger<TicketController> logger)
+        {
+			_logger = logger;
+		}
+        public Ticket GetTicketById(int id)
+		{
+			return new Ticket() { 
+				Id = id
+			};
+		}
+	}
+}
