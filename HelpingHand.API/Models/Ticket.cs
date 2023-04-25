d4b02c174cf2f459e11e0592f2f9e4ed4c2297ff
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-04-25 22:37:00 +0200

    add ticket model

---
+++ HelpingHand.API/Models/Ticket.cs
@@ -0,0 +1,11 @@
+﻿namespace HelpingHand.API.Models
+{
+	public class Ticket
+	{
+		public int Id { get; set; }
+		public string? Title { get; set; }
+		public string? Description { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public DateTime UpdatedDate { get; set; }
+	}
+}
