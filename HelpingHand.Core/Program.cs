fedd42e584b44d19d422d42e8b7f9aedfa79b558
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-04-25 19:10:41 +0200

    Add project files.

---
+++ HelpingHand.Core/Program.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace HelpingHand.Core
+{
+	public class Program
+	{
+		[ExcludeFromCodeCoverage]
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+
+			builder.Services.AddControllersWithViews();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseRouting();
+
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller}/{action=Index}/{id?}");
+
+			app.MapFallbackToFile("index.html");
+
+			app.Run();
+		}
+	}
+}
---
+++ HelpingHand.UnitTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+using HelpingHand.API;
+using HelpingHand.API.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace HelpingHand.UnitTests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			//Arrange
+			var logger = new Mock<ILogger<WeatherForecastController>>();
+			var controller = new WeatherForecastController(logger.Object);
+
+			//Act
+			var result = controller.Get();
+
+			//Assert
+			result.Should().BeOfType<WeatherForecast[]>();
+		}
+	}
+}
